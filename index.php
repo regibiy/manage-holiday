8e291c4da466661d76dd00e246d1c24dcd8c7ac4
regibiy <19412550_regi_r_b@widyadharma.ac.id>
2023-08-19 08:25:47 +0700

    added styles to index.php

--- index.php
+++ index.php
@@ -2,12 +2,32 @@
 include("connection.php");
 ?>
 
-<a href="add_data.php">Tambah hari libur di sini</a>
+<!DOCTYPE html>
+<html lang="en">
 
-<form action="action.php" method="post">
-    <div>
-        <label for="cek_tanggal">Cek tanggal</label>
-        <input type="date" name="cek_tanggal" id="cek_tanggal">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kelola Libur</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="btn">
+            <a href="add_data.php">Tambah hari libur di sini</a>
+        </div>
+
+        <div class="form">
+            <form action="action.php" method="post">
+                <div>
+                    <label for="cek_tanggal">Cek tanggal</label>
+                    <input type="date" name="cek_tanggal" id="cek_tanggal">
+                </div>
+                <button type="submit" name="cek">Cek Tanggal</button>
+            </form>
+        </div>
     </div>
-    <button type="submit" name="cek">Cek Tanggal</button>
-</form>
+</body>
+
+</html>
