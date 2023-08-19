b8b9bdfe0f5e94556206d6481e73a121e188dd9f
regibiy <19412550_regi_r_b@widyadharma.ac.id>
2023-08-19 07:34:33 +0700

    first release index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include("connection.php");
+?>
+
+<a href="add_data.php">Tambah hari libur</a>
+
+<form action="action.php" method="post">
+    <div>
+        <label for="cek_tanggal">Cek tanggal</label>
+        <input type="date" name="cek_tanggal" id="cek_tanggal">
+    </div>
+    <button type="submit" name="cek">Cek Tanggal</button>
+</form>
